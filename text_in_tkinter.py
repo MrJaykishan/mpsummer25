fb9a87ab5bf8ab6a4989df72eb8a9150201fd313
Techsrijan <aswanibtech@gmail.com>
2025-07-10 12:47:02 +0530

    notepad basic

--- text_in_tkinter.py
+++ text_in_tkinter.py
@@ -1,15 +1,17 @@
 from tkinter import *
-from tkinter import filedialog,messagebox
+from tkinter import filedialog,messagebox,colorchooser
+from turtledemo.colormixer import setbgcolor
+
 root=Tk()
 
-def new_file():
-    val = t.get(1.0, END)
-    print(val)
-    if val :
-        print("Kuch likha nahi hi")
-    else:
-        print(val)
-        print('kuch likha hi')
+def back():
+    c=colorchooser.askcolor()
+    t.config(background=c[1])
+    print(c)
+def fore():
+    c = colorchooser.askcolor()
+    t.config(foreground=c[1])
+    print(c)
 
 
 def get_data():
@@ -29,13 +31,36 @@ def save_file():
     s.write(data)
 
 def saveAs_file():
-    d=filedialog.asksaveasfilename(defaultextension="*.txt")
+    d=filedialog.asksaveasfile(defaultextension=".txt",mode='w')
     print(d)
+    data = t.get(1.0, END)
+    d.write(data)
+
+
+
+def new_file():
+    x = t.get(1.0, END)
+    if x.strip()=='':
+        pass
+    else:
+        print(x)
+        res = messagebox.askyesnocancel("Save File Confirmation", 'Do you want to Save this file?')
+        if res==True:
+            saveAs_file()
+        elif res==False:
+            t.delete(1.0, END)
+
+
+
 t=Text(root,width=15,wrap=WORD,selectbackground='red')
 # t.insert(INSERT,'hello how ar you\n')
 # t.insert(INSERT,'i am fine')
 # t.pack(fill=BOTH,expand=1)
 t.pack()
+btn_back=Button(root,text="background",command=back)
+btn_back.pack()
+btn_fore=Button(root,text="foreground",command=fore)
+btn_fore.pack()
 btn_new=Button(root,text="new",command=new_file)
 btn_new.pack()
 btn=Button(root,text="Clickme",command=get_data)
@@ -48,6 +73,6 @@ btn_save.pack()
 
 btn_saveAs=Button(root,text="Save AS file",command=saveAs_file)
 btn_saveAs.pack()
-root.geometry("600x500+200+100")
+root.geometry("600x800+200+100")
 # root.resizable(0,0)
 root.mainloop()
